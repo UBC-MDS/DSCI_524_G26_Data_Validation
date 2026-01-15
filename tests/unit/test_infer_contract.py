b6eb4211ed560d602ded6a70f1459c7575a9c21c
Eduardo Sanchez <ed.rasanmar@gmail.com>
2026-01-15 08:29:59 -0800

    Add 2nd test - Test function returns correct object (contract)

--- tests/unit/test_infer_contract.py
+++ tests/unit/test_infer_contract.py
@@ -1,10 +1,19 @@
 from data_validation.infer_contract import infer_contract
 import pytest
 import pandas as pd
+from data_validation.types import Contract
 
+#Test Error handling. Confirm error occurss if user input something other than a pandas DataFrame 
 def test_infer_contract_requires_dataframe():
     with pytest.raises(TypeError, match="pandas DataFrame"):
         infer_contract(None)
 
     with pytest.raises(TypeError, match="pandas DataFrame"):
         infer_contract("not a dataframe")
+
+
+def test_infer_contract_returns_contract():
+    df = pd.DataFrame({"a": [1, 2, 3]})
+    contract = infer_contract(df)
+
+    assert isinstance(contract, Contract)
